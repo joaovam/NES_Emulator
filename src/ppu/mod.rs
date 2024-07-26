618fa77e3c455b95f55d6170a71b722b83c630cf
joaovam <amorimvictorjoao3@gmail.com>
2024-07-26 16:42:11 -0300

    implement registers

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -1,5 +1,10 @@
 use crate::cartridge::Mirroring;
-
+use registers::control::ControlRegister;
+use registers::mask::{self, MaskRegister};
+use registers::status::StatusRegister;
+use registers::scroll::ScrollRegister;
+use registers::addr::AddrRegister;
+pub mod registers;
 pub trait PPU {
     fn write_to_ctrl(&mut self, value: u8);
     fn write_to_mask(&mut self, value: u8);
@@ -19,17 +24,26 @@ pub trait PPU {
 pub struct NesPPU{
     pub chr_rom: Vec<u8>,
     pub mirroring: Mirroring,
-    pub ctrl: ControlRegister,
-    addr: AddrRegister,
-
+    pub ctrl: ControlRegister, 
+    pub mask:MaskRegister,
+    pub status:StatusRegister,
+    pub scroll:ScrollRegister,
+    pub addr: AddrRegister, 
     pub palette_table: [u8;32],
     pub vram: [u8;2048],
+    pub oam_addr:u8,
     pub oam_data: [u8;256],
     internal_data_buf: u8,
     
 }
 
 impl NesPPU{
+
+    pub fn new_empty_rom() -> Self {
+        NesPPU::new(vec![0; 2048], Mirroring::HORIZONTAL)
+    }
+
+    
     pub fn new(chr_rom: Vec<u8>, mirroring: Mirroring)-> Self{
         NesPPU{
             chr_rom,
@@ -38,8 +52,13 @@ impl NesPPU{
             oam_data: [0; 64 * 4],
             palette_table: [0; 32],
             ctrl: ControlRegister::new(),
+            mask: MaskRegister::new(),
+            status: StatusRegister::new(),
+            scroll: ScrollRegister::new(),
             addr: AddrRegister::new(),
-            internal_data_buf: 0
+            oam_addr: 0,
+            internal_data_buf: 0,
+            
         }
     }
 
@@ -112,99 +131,246 @@ impl PPU for NesPPU{
             _ => panic!("Unexpected access to mirrored space {}", addr)
         }
     }
-}
-
-pub struct AddrRegister{
-    value: (u8,u8),
-    hi_ptr: bool,
-}
-
-impl AddrRegister{
-    pub fn new() -> Self{
-        AddrRegister { value: (0,0),
-             hi_ptr: true }
+    
+    fn write_to_mask(&mut self, value: u8) {
+        self.mask.update(value);   
     }
-
-    fn set(&mut self, data: u16){
-        self.value.0 = (data >> 8) as u8;
-        self.value.1 = (data & 0xff) as u8;
+    
+    fn read_status(&mut self) -> u8 {
+        let data = self.status.snapshot();
+        self.status.reset_vblank_status();
+        self.addr.reset_latch();
+        self.scroll.reset_latch();
+        data
     }
-
-    fn update(&mut self, data: u8){
-        if self.hi_ptr{
-            self.value.0 = data;
-        }else{
-            self.value.1 = data;
+    
+    fn write_to_oam_addr(&mut self, value: u8) {
+        self.oam_addr = value;
+    }
+    
+    fn write_to_oam_data(&mut self, value: u8) {
+        self.oam_data[self.oam_addr as usize] = value;
+        self.oam_addr = self.oam_addr.wrapping_add(1);
+    }
+    
+    fn read_oam_data(&self) -> u8 {
+        self.oam_data[self.oam_addr as usize]
+    }
+    
+    fn write_to_scroll(&mut self, value: u8) {
+        self.scroll.write(value);
+    }
+    
+    fn write_oam_dma(&mut self, value: &[u8; 256]) {
+        for x in value.iter(){
+            self.oam_data[self.oam_addr as usize] = *x;
+            self.oam_addr = self.oam_addr.wrapping_add(1);
         }
-        if self.get() > 0x3fff{
-            self.set(self.get() & 0b11111111111111);
-        }
-        self.hi_ptr = ! self.hi_ptr;
-    }
-
-    pub fn increment(&mut self, inc: u8){
-        let lo = self.value.1;
-        self.value.1 = self.value.1.wrapping_add(inc);
-        if lo > self.value.1{
-            self.value.0 = self.value.0.wrapping_add(1);
-        }
-        if self.get() > 0x3fff{
-            self.set(self.get() & 0b11111111111111)
-        }
-    }
-
-    pub fn reset_latch(&mut self){
-        self.hi_ptr = true;
-    }
-
-    pub fn get(&self) -> u16{
-        ((self.value.0 as u16) << 8) | (self.value.1 as u16)
     }
 }
 
 
-bitflags! {
 
-    // 7  bit  0
-    // ---- ----
-    // VPHB SINN
-    // |||| ||||
-    // |||| ||++- Base nametable address
-    // |||| ||    (0 = $2000; 1 = $2400; 2 = $2800; 3 = $2C00)
-    // |||| |+--- VRAM address increment per CPU read/write of PPUDATA
-    // |||| |     (0: add 1, going across; 1: add 32, going down)
-    // |||| +---- Sprite pattern table address for 8x8 sprites
-    // ||||       (0: $0000; 1: $1000; ignored in 8x16 mode)
-    // |||+------ Background pattern table address (0: $0000; 1: $1000)
-    // ||+------- Sprite size (0: 8x8 pixels; 1: 8x16 pixels)
-    // |+-------- PPU master/slave select
-    // |          (0: read backdrop from EXT pins; 1: output color on EXT pins)
-    // +--------- Generate an NMI at the start of the
-    //            vertical blanking interval (0: off; 1: on)
-    pub struct ControlRegister: u8 {
-        const NAMETABLE1              = 0b00000001;
-        const NAMETABLE2              = 0b00000010;
-        const VRAM_ADD_INCREMENT      = 0b00000100;
-        const SPRITE_PATTERN_ADDR     = 0b00001000;
-        const BACKROUND_PATTERN_ADDR  = 0b00010000;
-        const SPRITE_SIZE             = 0b00100000;
-        const MASTER_SLAVE_SELECT     = 0b01000000;
-        const GENERATE_NMI            = 0b10000000;
-    }
- }
 
-impl ControlRegister{
-    pub fn new() -> Self{
-        ControlRegister::from_bits_truncate(0b00000000)
+#[cfg(test)]
+pub mod test {
+    use super::*;
+
+    #[test]
+    fn test_ppu_vram_writes() {
+        let mut ppu = NesPPU::new_empty_rom();
+        ppu.write_to_ppu_addr(0x23);
+        ppu.write_to_ppu_addr(0x05);
+        ppu.write_to_data(0x66);
+
+        assert_eq!(ppu.vram[0x0305], 0x66);
     }
-    pub fn vram_addr_increment(&self) -> u8{
-        if !self.contains(ControlRegister::VRAM_ADD_INCREMENT){
-            1
-        }else{
-            32
-        }
+
+    #[test]
+    fn test_ppu_vram_reads() {
+        let mut ppu = NesPPU::new_empty_rom();
+        ppu.write_to_ctrl(0);
+        ppu.vram[0x0305] = 0x66;
+
+        ppu.write_to_ppu_addr(0x23);
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.read_data(); //load_into_buffer
+        assert_eq!(ppu.addr.get(), 0x2306);
+        assert_eq!(ppu.read_data(), 0x66);
     }
-    pub fn update(&mut self, data: u8){
-        self.bits = data;
+
+    #[test]
+    fn test_ppu_vram_reads_cross_page() {
+        let mut ppu = NesPPU::new_empty_rom();
+        ppu.write_to_ctrl(0);
+        ppu.vram[0x01ff] = 0x66;
+        ppu.vram[0x0200] = 0x77;
+
+        ppu.write_to_ppu_addr(0x21);
+        ppu.write_to_ppu_addr(0xff);
+
+        ppu.read_data(); //load_into_buffer
+        assert_eq!(ppu.read_data(), 0x66);
+        assert_eq!(ppu.read_data(), 0x77);
+    }
+
+    #[test]
+    fn test_ppu_vram_reads_step_32() {
+        let mut ppu = NesPPU::new_empty_rom();
+        ppu.write_to_ctrl(0b100);
+        ppu.vram[0x01ff] = 0x66;
+        ppu.vram[0x01ff + 32] = 0x77;
+        ppu.vram[0x01ff + 64] = 0x88;
+
+        ppu.write_to_ppu_addr(0x21);
+        ppu.write_to_ppu_addr(0xff);
+
+        ppu.read_data(); //load_into_buffer
+        assert_eq!(ppu.read_data(), 0x66);
+        assert_eq!(ppu.read_data(), 0x77);
+        assert_eq!(ppu.read_data(), 0x88);
+    }
+
+    // Horizontal: https://wiki.nesdev.com/w/index.php/Mirroring
+    //   [0x2000 A ] [0x2400 a ]
+    //   [0x2800 B ] [0x2C00 b ]
+    #[test]
+    fn test_vram_horizontal_mirror() {
+        let mut ppu = NesPPU::new_empty_rom();
+        ppu.write_to_ppu_addr(0x24);
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.write_to_data(0x66); //write to a
+
+        ppu.write_to_ppu_addr(0x28);
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.write_to_data(0x77); //write to B
+
+        ppu.write_to_ppu_addr(0x20);
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.read_data(); //load into buffer
+        assert_eq!(ppu.read_data(), 0x66); //read from A
+
+        ppu.write_to_ppu_addr(0x2C);
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.read_data(); //load into buffer
+        assert_eq!(ppu.read_data(), 0x77); //read from b
+    }
+
+    // Vertical: https://wiki.nesdev.com/w/index.php/Mirroring
+    //   [0x2000 A ] [0x2400 B ]
+    //   [0x2800 a ] [0x2C00 b ]
+    #[test]
+    fn test_vram_vertical_mirror() {
+        let mut ppu = NesPPU::new(vec![0; 2048], Mirroring::VERTICAL);
+
+        ppu.write_to_ppu_addr(0x20);
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.write_to_data(0x66); //write to A
+
+        ppu.write_to_ppu_addr(0x2C);
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.write_to_data(0x77); //write to b
+
+        ppu.write_to_ppu_addr(0x28);
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.read_data(); //load into buffer
+        assert_eq!(ppu.read_data(), 0x66); //read from a
+
+        ppu.write_to_ppu_addr(0x24);
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.read_data(); //load into buffer
+        assert_eq!(ppu.read_data(), 0x77); //read from B
+    }
+
+    #[test]
+    fn test_read_status_resets_latch() {
+        let mut ppu = NesPPU::new_empty_rom();
+        ppu.vram[0x0305] = 0x66;
+
+        ppu.write_to_ppu_addr(0x21);
+        ppu.write_to_ppu_addr(0x23);
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.read_data(); //load_into_buffer
+        assert_ne!(ppu.read_data(), 0x66);
+
+        ppu.read_status();
+
+        ppu.write_to_ppu_addr(0x23);
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.read_data(); //load_into_buffer
+        assert_eq!(ppu.read_data(), 0x66);
+    }
+
+    #[test]
+    fn test_ppu_vram_mirroring() {
+        let mut ppu = NesPPU::new_empty_rom();
+        ppu.write_to_ctrl(0);
+        ppu.vram[0x0305] = 0x66;
+
+        ppu.write_to_ppu_addr(0x63); //0x6305 -> 0x2305
+        ppu.write_to_ppu_addr(0x05);
+
+        ppu.read_data(); //load into_buffer
+        assert_eq!(ppu.read_data(), 0x66);
+        // assert_eq!(ppu.addr.read(), 0x0306)
+    }
+
+    #[test]
+    fn test_read_status_resets_vblank() {
+        let mut ppu = NesPPU::new_empty_rom();
+        ppu.status.set_vblank_status(true);
+
+        let status = ppu.read_status();
+
+        assert_eq!(status >> 7, 1);
+        assert_eq!(ppu.status.snapshot() >> 7, 0);
+    }
+
+    #[test]
+    fn test_oam_read_write() {
+        let mut ppu = NesPPU::new_empty_rom();
+        ppu.write_to_oam_addr(0x10);
+        ppu.write_to_oam_data(0x66);
+        ppu.write_to_oam_data(0x77);
+
+        ppu.write_to_oam_addr(0x10);
+        assert_eq!(ppu.read_oam_data(), 0x66);
+
+        ppu.write_to_oam_addr(0x11);
+        assert_eq!(ppu.read_oam_data(), 0x77);
+    }
+
+    #[test]
+    fn test_oam_dma() {
+        let mut ppu = NesPPU::new_empty_rom();
+
+        let mut data = [0x66; 256];
+        data[0] = 0x77;
+        data[255] = 0x88;
+
+        ppu.write_to_oam_addr(0x10);
+        ppu.write_oam_dma(&data);
+
+        ppu.write_to_oam_addr(0xf); //wrap around
+        assert_eq!(ppu.read_oam_data(), 0x88);
+
+        ppu.write_to_oam_addr(0x10);
+        assert_eq!(ppu.read_oam_data(), 0x77);
+  
+        ppu.write_to_oam_addr(0x11);
+        assert_eq!(ppu.read_oam_data(), 0x66);
     }
 }
+
+
